2efd535b66a8f830a1e27800f9c9228cfeeaf8ad
Schmulik Raskin <schmuli@gmail.com>
2016-04-12 12:39:37 +0300

    (spec) Add public API spec

---
+++ spec/public_api_spec.ts
@@ -0,0 +1,20 @@
+import publicApi from "../lib/project";
+
+describe("The project public API", () => {
+
+    it('should have a version property', () => {
+        expect(publicApi.version).toBe('0.0.0');
+    });
+
+    it('should have a new command');
+
+    it('should have an init command');
+
+    it('should have a deps command');
+
+    it('should have a test command');
+
+    it('should have a build command');
+
+    it('should have a deploy command');
+});
